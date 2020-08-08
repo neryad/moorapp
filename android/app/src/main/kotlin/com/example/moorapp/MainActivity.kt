922a3c5590afd09e603b73d58309dbdc99356f60
Dayern Gomez <dayern.gomez@hotmail.com>
2020-08-08 18:50:21 -0400

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/moorapp/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.moorapp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,110 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+import 'data/morr_database.dart';
+
+
+void main() {
+  runApp(Orders());
+}
+
+class Orders extends StatefulWidget {
+  @override
+  _OrdersState createState() => _OrdersState();
+}
+
+class _OrdersState extends State<Orders> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      //theme: ThemeData.dark(),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Orders List"),
+        ),
+        body: SingleChildScrollView(child: NewOrder()),
+      ),
+    );
+  }
+}
+
+class NewOrder extends StatefulWidget {
+  @override
+  _NewOrderState createState() => _NewOrderState();
+}
+
+class _NewOrderState extends State<NewOrder> {
+  TextEditingController productNameController = TextEditingController();
+
+  TextEditingController priceController = TextEditingController();
+  bool isloading = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        TextField(
+          decoration: InputDecoration(hintText: 'Product Name'),
+          keyboardType: TextInputType.text,
+          controller: productNameController,
+        ),
+        TextField(
+          decoration: InputDecoration(hintText: 'Product Price'),
+          keyboardType: TextInputType.number,
+          controller: priceController,
+        ),
+        RaisedButton(
+          onPressed: () {
+            setState(() {
+              AppDatabase().insertNewOrder(Order(
+                price: priceController.text,
+                productName: productNameController.text,
+              ));
+
+              priceController.clear();
+              productNameController.clear();
+            });
+          },
+          //color: Colors.greenAccent,
+          child: Text("Place Order"),
+        ),
+        Container(
+          height: 700,
+          width: double.infinity,
+          child: StreamBuilder(
+            stream: AppDatabase().watchAllOrder(),
+            builder: (context, AsyncSnapshot<List<Order>> snapshot) {
+              return ListView.builder(
+                itemBuilder: (_, index) {
+                  return Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Card(
+                      child: ListTile(
+                          leading: CircleAvatar(
+                            child: Text('${index + 1}', style: TextStyle(color: Colors.white)),
+                            radius: 20,
+                          ),
+                          title: Text(snapshot.data[index].productName),
+                          subtitle: Text("Rs. ${snapshot.data[index].price}"),
+                          trailing: IconButton(
+                            icon: Icon(Icons.delete_outline),
+                            onPressed: () {
+                              setState(() {
+                                AppDatabase().deleteOrder(snapshot.data[index]);
+                              });
+                            },
+                            color: Colors.red,
+                          )),
+                    ),
+                  );
+                },
+                itemCount: snapshot.data.length,
+              );
+            },
+          ),
+        )
+      ],
+    );
+  }
+}
